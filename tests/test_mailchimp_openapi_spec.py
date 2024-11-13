04bcffb5f149a052ed6d250aad1dada2afe75871
Robert Jambrecic <robert@airt.ai>
2024-11-13 13:17:23 +0000

    wip

--- tests/test_mailchimp_openapi_spec.py
+++ tests/test_mailchimp_openapi_spec.py
@@ -1,29 +1,38 @@
-from collections.abc import Iterator
+from unittest.mock import MagicMock, patch
 
+import pytest
 from autogen import UserProxyAgent
 from fastagency.api.openapi.client import OpenAPI
 
 from mailchimp_api.utils.create_api import create_api
 
-from unittest.mock import MagicMock, patch
-import pytest
 
 @pytest.fixture(scope="class")
 def setup_api() -> OpenAPI:
-    return create_api(mailchimp_api_key="test Key") # pragma: allowlist secret
+    return create_api(mailchimp_api_key="test Key")  # pragma: allowlist secret
+
 
 class TestMailchimpOpenapiSpec:
+    @pytest.fixture(autouse=True)
+    def _setup(
+        self,
+    ) -> None:
+        self.headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Basic YW55c3RyaW5nOnRlc3QgS2V5",
+        }
+        return
 
     def test_create_api(self, setup_api: OpenAPI) -> None:
         assert isinstance(setup_api, OpenAPI)
         url = setup_api.servers[0]["url"]
         assert url == "https://us14.api.mailchimp.com/3.0", url
 
-    @patch("fastagency.api.openapi.client.requests.get")
-    def test_mailchimp_api(
+    def _test_mailchimp_api(
         self,
         mock_post: MagicMock,
         setup_api: OpenAPI,
+        function: str,
     ) -> None:
         mock_response = MagicMock()
         mock_response.status_code = 200
@@ -36,19 +45,27 @@ class TestMailchimpOpenapiSpec:
             code_execution_config=False,
         )
 
-        functions = ["get_ping"]
-        setup_api._register_for_execution(user_proxy, functions=functions)
+        setup_api._register_for_execution(user_proxy, functions=[function])
 
-        assert tuple(user_proxy._function_map.keys()) == ("get_ping",)
+        assert tuple(user_proxy._function_map.keys()) == (function,)
 
-        get_ping = user_proxy._function_map["get_ping"]
-        get_ping(body={})
+        func = user_proxy._function_map[function]
+        func(body={})
+
+    @patch("fastagency.api.openapi.client.requests.get")
+    def test_mailchimp_api(
+        self,
+        mock_post: MagicMock,
+        setup_api: OpenAPI,
+    ) -> None:
+        self._test_mailchimp_api(
+            mock_post=mock_post,
+            setup_api=setup_api,
+            function="get_ping",
+        )
 
         mock_post.assert_called_once_with(
             f"{setup_api.servers[0]['url']}/ping",
             params={},
-            headers={
-                'Content-Type': 'application/json',
-                'Authorization': 'Basic YW55c3RyaW5nOnRlc3QgS2V5',
-            },
+            headers=self.headers,
         )
