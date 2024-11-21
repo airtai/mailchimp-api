c6302419e108254116e0da75708b450d9583b392
Robert Jambrecic <robert@airt.ai>
2024-11-21 14:57:06 +0000

    Implement workflow

--- mailchimp_api/workflow.py
+++ mailchimp_api/workflow.py
@@ -27,8 +27,20 @@ def _get_config() -> Config:
 config = _get_config()
 
 
+def _wait_for_file(timestamp: str) -> pd.DataFrame:
+    file_name = f"uploaded-file-{timestamp}.csv"
+    file_path = UPLOADED_FILES_DIR / file_name
+    while not file_path.exists():
+        time.sleep(2)
+
+    df = pd.read_csv(file_path)
+    file_path.unlink()
+
+    return df
+
+
 @wf.register(name="mailchimp_chat", description="Mailchimp tags update chat")  # type: ignore[misc]
-def simple_workflow(ui: UI, params: dict[str, Any]) -> str:
+def mailchimp_chat(ui: UI, params: dict[str, Any]) -> str:
     timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
     body = f"""Please upload **.csv** file with the email addresses for which you want to update the tags.
 
@@ -40,13 +52,7 @@ def simple_workflow(ui: UI, params: dict[str, Any]) -> str:
         body=body,
     )
 
-    file_name = f"uploaded-file-{timestamp}.csv"
-    file_path = UPLOADED_FILES_DIR / file_name
-    while not file_path.exists():
-        time.sleep(2)
-
-    df = pd.read_csv(file_path)
-    file_path.unlink()
+    df = _wait_for_file(timestamp)
 
     list_name = None
     while list_name is None:
@@ -59,5 +65,22 @@ def simple_workflow(ui: UI, params: dict[str, Any]) -> str:
     add_tag_members, _ = update_tags(
         crm_df=df, config=config, list_name=list_name.strip()
     )
+    if not add_tag_members:
+        return "No tags added"
 
-    return f"Added tags\n{add_tag_members}"
+    add_tag_members = dict(sorted(add_tag_members.items()))
+    updates_per_tag = "\n".join(
+        [f"- **{key}**: {len(value)}" for key, value in add_tag_members.items()]
+    )
+    body = f"""Number of updates per tag:
+
+{updates_per_tag}
+
+(It might take some time for updates to reflect in Mailchimp)
+"""
+    ui.text_message(
+        sender="Workflow",
+        recipient="User",
+        body=body,
+    )
+    return "Task Completed"
